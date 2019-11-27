324e7e1205eb362847396c68eea88fde050d1ec8
Pasquale Paudice <pasquale.paudice10@gmail.com>
2019-11-27 14:54:47 +0100

    slider next e slider prev impostati sui pallini

--- main.js
+++ main.js
@@ -5,17 +5,24 @@ $(document).ready(function(){
     $('.next').click(function(){
 
         var img_attuale = $('img.active');
+        var pal_attuale = $('span.active');
 
         var img_successiva = img_attuale.next('img');
+        var pal_successivo = pal_attuale.next('span');
 
         img_attuale.removeClass('active');
 
+        pal_attuale.removeClass('active');
+
         if (img_successiva.length == 0) {
             img_successiva = $('img.first');
+
+            pal_successivo = $('span.first');
         }
 
 
         img_successiva.addClass('active');
+        pal_successivo.addClass('active');
 
 
     })
@@ -25,18 +32,23 @@ $(document).ready(function(){
     $('.prev').click(function(){
 
         var img_attuale = $('img.active');
+        var pal_attuale = $('span.active');
 
         var img_successiva = img_attuale.prev('img');
+        var pal_successivo = pal_attuale.prev('span');
 
         img_attuale.removeClass('active');
 
+        pal_attuale.removeClass('active');
+
         if (img_successiva.length == 0) {
             img_successiva = $('img.last');
+            pal_successivo = $('span.last');
         }
 
 
         img_successiva.addClass('active');
-
+        pal_successivo.addClass('active');
 
 
     })
